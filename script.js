1b19b2e4fa33f59a4691351ad555fd8ddd5f7975
natalysilveira <naty.silveira01@gmail.com>
2022-06-13 21:34:58 -0400

    Atividade realizada

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Maria"
+let sobrenome = "Silva"
+let anoNascimento = 1990
+let cidade = "Manaus"
+let anoAtual = 2022
+console.log(`Eu sou ${nome} ${sobrenome}, tenho ${(anoAtual-anoNascimento)} anos e moro em ${cidade}.`)
